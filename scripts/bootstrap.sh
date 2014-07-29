537dd59febfd0a0a5c5a2ca9efaee1c636bea30a
Peter Jenkins <peter@funroll.co>
2014-07-29 11:31:13 -0400

    Add a description of what this file is supposed to do.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+# Purpose: This script is intended to be the minimum amount of shell necessary so further development can be done in nodejs coffeescript.
+
 #TODO: Install nodejs so the following line will work
 # sudo npm install -g coffee-script
